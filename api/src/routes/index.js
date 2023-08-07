b15eb134126771900283d21c248a99778f66c430
Fausto Lalia <lmfaustolm@gmail.com>
2023-08-07 20:32:34 -0300

    prePull2

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {getProducts, obtenerProductoPorId, crearProducto, buscarProductos, getProductsAvailable, getProductsUnavailable } = require("../controllers/productControllers")
-const {getUsers, getAllUsers, obtenerUserPorId, obtenerUserPorSub, crearUser, actualizarUser} = require("../controllers/userControllers")
+const {getUsers, obtenerUserPorId, crearUser, actualizarUser} = require("../controllers/userControllers")
+const { createPaymentPreference } = require("../controllers/payamentContoller")
 
 router.get("/users" , getUsers)
-router.get("/users/:id" , obtenerUserPorSub)
-// router.get("/users/:id" , obtenerUserPorId)
+router.get("/users/:id" , obtenerUserPorId)
 router.post("/users", crearUser)
 router.put('/users/:id', actualizarUser);
 
@@ -19,4 +19,3 @@ router.get("/nodisponible", getProductsUnavailable)
 router.post('/create-order', createPaymentPreference);
 
 module.exports = router;
- 
